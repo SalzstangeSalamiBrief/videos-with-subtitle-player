91484461937bbbdc6316e965180b3a23a8ffa0ce
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2024-08-02 16:19:51 +0200

    Fix paths used in Test_getPartsOfPath

--- backend/useCases/utilities/getPartsOfPath_test.go
+++ backend/useCases/utilities/getPartsOfPath_test.go
@@ -2,14 +2,15 @@ package usecases
 
 import (
 	"backend/models"
+	"path/filepath"
 	"testing"
 )
 
-func Test_getPartsOfPath(t *testing.T) {
+func Test_GetPartsOfPath(t *testing.T) {
 	inputs := []models.TestData[string, []string]{
 		{Title: "Should return an empty array on empty input", Input: "", Expected: []string{}},
-		{Title: "Should return an array with three items", Input: "t1\\t2\\t3\\file.mp3", Expected: []string{"t1", "t2", "t3"}},
-		{Title: "Should return an array with one item", Input: "t1\\file.mp3", Expected: []string{"t1"}},
+		{Title: "Should return an array with three items", Input: filepath.Join("t1", "t2", "t3", "file.mp3"), Expected: []string{"t1", "t2", "t3"}},
+		{Title: "Should return an array with one item", Input: filepath.Join("t1", "file.mp3"), Expected: []string{"t1"}},
 		{Title: "Should return an empty array with only file name", Input: "file.mp3", Expected: []string{}},
 	}
 
