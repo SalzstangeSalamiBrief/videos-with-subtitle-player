25ad2cb634e6877fd3624d9151e745c812ca3988
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2024-10-01 12:40:01 +0200

    Create FolderList.tsx, Repalce some usage of FOlerListSection with FOlderList and custom guards

--- frontend/src/features/folderListSection/FolderListSection.tsx
+++ frontend/src/features/folderListSection/FolderListSection.tsx
@@ -1,8 +1,5 @@
-import { baseLinkStyles } from '$lib/styles/baseLinkStyles';
-import { getImageUrlForId } from '$lib/utilities/getImageUrl';
 import { IFileTree } from '$models/fileTree';
-import { ImageCard } from '$sharedComponents/card/ImageCard';
-import { Link as TanStackRouterLink } from '@tanstack/react-router';
+import { FolderList } from '$sharedComponents/folderList/FolderList';
 
 interface IFolderListSectionProps {
   folders: IFileTree[];
@@ -13,22 +10,5 @@ export function FolderListSection({ folders }: IFolderListSectionProps) {
     return <p>This folder contains no subfolders</p>;
   }
 
-  return (
-    <ul className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {folders.map((child) => (
-        <li key={child.id}>
-          <TanStackRouterLink
-            to="/folders/$folderId"
-            params={{ folderId: child.id }}
-            className={baseLinkStyles}
-          >
-            <ImageCard
-              title={child.name}
-              imageUrl={getImageUrlForId(child.thumbnailId)}
-            />
-          </TanStackRouterLink>
-        </li>
-      ))}
-    </ul>
-  );
+  return <FolderList folders={folders} />;
 }
---
+++ frontend/src/sharedComponents/folderList/FolderList.tsx
@@ -0,0 +1,30 @@
+import { baseLinkStyles } from '$lib/styles/baseLinkStyles';
+import { getImageUrlForId } from '$lib/utilities/getImageUrl';
+import { IFileTree } from '$models/fileTree';
+import { ImageCard } from '$sharedComponents/card/ImageCard';
+import { Link as TanStackRouterLink } from '@tanstack/react-router';
+
+interface IFolderListSectionProps {
+  folders: IFileTree[];
+}
+
+export function FolderList({ folders }: IFolderListSectionProps) {
+  return (
+    <ul className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {folders.map((child) => (
+        <li key={child.id}>
+          <TanStackRouterLink
+            to="/folders/$folderId"
+            params={{ folderId: child.id }}
+            className={baseLinkStyles}
+          >
+            <ImageCard
+              title={child.name}
+              imageUrl={getImageUrlForId(child.thumbnailId)}
+            />
+          </TanStackRouterLink>
+        </li>
+      ))}
+    </ul>
+  );
+}
