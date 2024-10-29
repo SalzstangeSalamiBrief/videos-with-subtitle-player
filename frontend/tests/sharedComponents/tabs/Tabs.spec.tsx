d1381eac77445eb3b7ef9d90634a9a2d32416657
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2024-10-29 12:31:25 +0100

    fix

--- frontend/tests/sharedComponents/tabs/Tabs.spec.tsx
+++ frontend/tests/sharedComponents/tabs/Tabs.spec.tsx
@@ -1,73 +1,74 @@
 import { ITab, Tabs } from '$sharedComponents/tabs/Tabs';
 import { render, screen, within } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
+describe('Tabs', () => {
+  test('Renders nothing if no tabs are provided', () => {
+    const { container } = render(
+      <Tabs label="Test" activeTabIndex={0} onChangeTab={() => {}} tabs={[]} />,
+    );
+    expect(container.innerHTML).toBe('');
+  });
 
-test('Renders nothing if no tabs are provided', () => {
-  const { container } = render(
-    <Tabs label="Test" activeTabIndex={0} onChangeTab={() => {}} tabs={[]} />,
-  );
-  expect(container.innerHTML).toBe('');
-});
+  test('Render one tab with content', () => {
+    const sectionLabel = 'Sectionlabel';
+    const tabContent = 'tabContent';
+    const singleTab: ITab = { label: 'Test', content: <div>{tabContent}</div> };
+    render(
+      <Tabs
+        label={sectionLabel}
+        activeTabIndex={0}
+        onChangeTab={() => {}}
+        tabs={[singleTab]}
+      />,
+    );
 
-test('Render one tab with content', () => {
-  const sectionLabel = 'Sectionlabel';
-  const tabContent = 'tabContent';
-  const singleTab: ITab = { label: 'Test', content: <div>{tabContent}</div> };
-  render(
-    <Tabs
-      label={sectionLabel}
-      activeTabIndex={0}
-      onChangeTab={() => {}}
-      tabs={[singleTab]}
-    />,
-  );
+    expect(screen.getByText(sectionLabel)).toBeInTheDocument();
+    const tablist = screen.getByRole('tablist');
+    const tabs = within(tablist).getAllByRole('tab');
+    expect(tabs.length).toBe(1);
+    expect(tabs[0]).toHaveTextContent(singleTab.label);
+    const tabpanel = within(tablist).getByRole('tabpanel');
+    expect(tabpanel).toHaveTextContent(tabContent);
+  });
 
-  expect(screen.getByText(sectionLabel)).toBeInTheDocument();
-  const tablist = screen.getByRole('tablist');
-  const tabs = within(tablist).getAllByRole('tab');
-  expect(tabs.length).toBe(1);
-  expect(tabs[0]).toHaveTextContent(singleTab.label);
-  const tabpanel = within(tablist).getByRole('tabpanel');
-  expect(tabpanel).toHaveTextContent(tabContent);
-});
+  describe('Render multiple tabs', () => {
+    const differentTabsTestCases = Array.from({ length: 5 }).map<{
+      label: string;
+      content: string;
+    }>((_, i) => ({ label: `Tab ${i}`, content: `Content ${i}` }));
 
-describe('Render multiple tabs', () => {
-  const differentTabsTestCases = Array.from({ length: 5 }).map<{
-    label: string;
-    content: string;
-  }>((_, i) => ({ label: `Tab ${i}`, content: `Content ${i}` }));
-
-  test.each(differentTabsTestCases)(
-    'Render multiple tabs with active tab $label',
-    ({ label, content }) => {
-      const i = differentTabsTestCases.findIndex(
-        (tab) => tab.label === label && tab.content === content,
-      );
-      render(
-        <Tabs
-          label="SectionLabel"
-          activeTabIndex={i}
-          onChangeTab={() => {}}
-          tabs={differentTabsTestCases.map(({ label, content }) => ({
-            label,
-            content: <div>{content}</div>,
-          }))}
-        />,
-      );
-
-      const tablist = screen.getByRole('tablist');
-      const tabs = within(tablist).getAllByRole('tab');
-      expect(tabs.length).toBe(differentTabsTestCases.length);
-      tabs.forEach((tab, j) => {
-        expect(tab).toHaveTextContent(differentTabsTestCases[j].label);
-        expect(tab).toHaveAttribute(
-          'aria-selected',
-          j === i ? 'true' : 'false',
+    test.each(differentTabsTestCases)(
+      'Render multiple tabs with active tab $label',
+      ({ label, content }) => {
+        const i = differentTabsTestCases.findIndex(
+          (tab) => tab.label === label && tab.content === content,
         );
-      });
+        render(
+          <Tabs
+            label="SectionLabel"
+            activeTabIndex={i}
+            onChangeTab={() => {}}
+            tabs={differentTabsTestCases.map(({ label, content }) => ({
+              label,
+              content: <div>{content}</div>,
+            }))}
+          />,
+        );
 
-      const tabpanel = within(tablist).getByRole('tabpanel');
-      expect(tabpanel).toHaveTextContent(content);
-    },
-  );
+        const tablist = screen.getByRole('tablist');
+        const tabs = within(tablist).getAllByRole('tab');
+        expect(tabs.length).toBe(differentTabsTestCases.length);
+        tabs.forEach((tab, j) => {
+          expect(tab).toHaveTextContent(differentTabsTestCases[j].label);
+          expect(tab).toHaveAttribute(
+            'aria-selected',
+            j === i ? 'true' : 'false',
+          );
+        });
+
+        const tabpanel = within(tablist).getByRole('tabpanel');
+        expect(tabpanel).toHaveTextContent(content);
+      },
+    );
+  });
 });
