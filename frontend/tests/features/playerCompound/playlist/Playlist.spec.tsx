c842461280540bd248fa6770eaa43177720095e4
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2024-10-29 12:55:51 +0100

    Add comment to playlist.spec

--- frontend/tests/features/playerCompound/playlist/Playlist.spec.tsx
+++ frontend/tests/features/playerCompound/playlist/Playlist.spec.tsx
@@ -5,19 +5,21 @@
  * !    src/routeTree.gen.ts:49:53
  */
 
+import { describe, test } from 'vitest';
+
 // import { Playlist } from '$features/playerCompound/playlist/Playlist';
 // import { screen, within } from '@testing-library/react';
 // import { describe, expect, test } from 'vitest';
 // import { RenderFakeRouterShell } from '../../../utlities/RenderFakeRouterShell';
 
 // // COMPONENTS THAT USE TANSTACK ROUTER CANNOT BE TESTED AT THE CURRENT STATE
-// describe.skip('Playlist', () => {
-//   test('Render only header if no siblings exist', () => {
-//     RenderFakeRouterShell(() => <Playlist siblings={[]} />);
-//     expect(screen.getByText('Playlist')).toBeInTheDocument();
-//     const list = screen.queryByRole('list');
-//     expect(list).toBeInTheDocument();
-//     const listItems = within(list!).queryAllByRole('listitem');
-//     expect(listItems.length).toBe(0);
-//   });
-// });
+describe.skip('Playlist', () => {
+  test('Render only header if no siblings exist', () => {
+    //     RenderFakeRouterShell(() => <Playlist siblings={[]} />);
+    //     expect(screen.getByText('Playlist')).toBeInTheDocument();
+    //     const list = screen.queryByRole('list');
+    //     expect(list).toBeInTheDocument();
+    //     const listItems = within(list!).queryAllByRole('listitem');
+    //     expect(listItems.length).toBe(0);
+  });
+});
