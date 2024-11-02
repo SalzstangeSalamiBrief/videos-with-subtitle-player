1ecfacee327af48f5fc4689fd898b33ba3134dc2
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2024-11-02 12:31:11 +0000

    add todos to appconfig

--- backend/pkg/config/appConfig.go
+++ backend/pkg/config/appConfig.go
@@ -7,7 +7,8 @@ type AppConfig struct {
 }
 
 var AppConfiguration AppConfig
-
+// TODO build a custom solution that reads an env file
+// TODO the custom solution generates a struct for the content and type safety
 func InitializeConfiguration() {
 	AppConfiguration.RootPath = os.Getenv("ROOT_PATH")
 }
