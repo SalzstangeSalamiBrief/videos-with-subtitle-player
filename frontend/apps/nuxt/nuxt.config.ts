1dd669cfa146a68d2cdffb168fb8268a60ae73d5
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2025-05-30 14:23:09 +0200

    Add daisy ui to nuxt app; Set port of dev server to 4200

--- frontend/apps/nuxt/nuxt.config.ts
+++ frontend/apps/nuxt/nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
-  compatibilityDate: "2025-05-15",
+  compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/test-utils"],
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/test-utils'],
+  devServer: {
+    port: 4200,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  css: ['~/assets/app.css'],
 });
