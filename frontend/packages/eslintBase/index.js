81f6a43b1bc07640cab8bcf28190d9503afba912
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2025-05-30 10:30:55 +0200

    fix import of prettier-base

--- frontend/packages/eslintBase/index.js
+++ frontend/packages/eslintBase/index.js
@@ -1,24 +1,24 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
-  { ignores: ['dist', 'api.ts'] },
+  { ignores: ["dist", "api.ts"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     rules: {
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
 
         { allowConstantExport: true },
       ],
-      '@typescript-eslint/no-explicit-any': 'error',
-      'no-console': ['error', { allow: ['warn', 'error'] }],
+      "@typescript-eslint/no-explicit-any": "error",
+      "no-console": ["error", { allow: ["warn", "error"] }],
     },
   },
-]
+];
