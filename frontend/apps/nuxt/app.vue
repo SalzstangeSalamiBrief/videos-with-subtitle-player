d8cb6ea21be79078979be7a7d0825a85fecd30b0
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2025-06-01 11:12:20 +0200

    Add code for fixing scroll issues on page change

--- frontend/apps/nuxt/app.vue
+++ frontend/apps/nuxt/app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+// TODO COMMENT IN TO FIX NOT SCROLLING TO TOP ON PAGE CHANGE?
+// const nuxtApp = useNuxtApp();
+
+// nuxtApp.hook('page:finish', () => {
+//   window.scrollTo(0, 0);
+// });
+</script>
+
 <template>
   <NuxtLayout name="default">
     <NuxtPage />
