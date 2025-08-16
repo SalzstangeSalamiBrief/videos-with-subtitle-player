80d4613f284dcf96b38648b2fbbcd0d82af13124
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2025-08-16 14:39:56 +0200

    Remove code that is commented out

--- backend/pkg/api/handlers/getContinuousFileByIdHandler.go
+++ backend/pkg/api/handlers/getContinuousFileByIdHandler.go
@@ -68,53 +68,6 @@ func NewGetContinuousFileByIdHandler(configuration ContinuousFileByIdHandlerConf
 	}
 }
 
-//func GetContinuousFileByIdHandler(w http.ResponseWriter, r *http.Request) {
-//	fileIdString := strings.TrimPrefix(r.URL.Path, "/api/file/continuous/")
-//	continuousFileInTree := utilities.GetFileByIdAndExtension(fileTreeManager.FileTreeItems, fileIdString, constants.AllowedContinuousFileExtensions...)
-//	if continuousFileInTree.Id == "" {
-//		ErrorHandler(w, fmt.Sprintf("Could not get resource %v", fileIdString), http.StatusBadRequest)
-//		return
-//	}
-//
-//	filePathOnHardDisk := path.Join(config.AppConfiguration.RootPath, continuousFileInTree.Path)
-//	file, err := os.Open(filePathOnHardDisk)
-//	defer file.Close()
-//	if err != nil {
-//		fmt.Println(err.Error())
-//		ErrorHandler(w, fmt.Sprintf("Could not get resource '%v'", fileIdString), http.StatusInternalServerError)
-//		return
-//	}
-//
-//	fileSize, err := getFileSize(file)
-//	if err != nil {
-//		fmt.Println(err.Error())
-//		ErrorHandler(w, fmt.Sprintf("Could not get resource '%v'", fileIdString), http.StatusInternalServerError)
-//		return
-//	}
-//
-//	rangeHeaderWithPrefix := r.Header.Get("Range")
-//	start, end := utilities.GetRequestedRangesFromHeaderField(utilities.GetRequestRangesInput{RangeHeaderWithPrefix: rangeHeaderWithPrefix, ChunkSize: chunkSize, FileSize: fileSize})
-//	if start == 0 && end == 0 {
-//		ErrorHandler(w, fmt.Sprintf("The request does not contain a range header for file '%v'", fileIdString), http.StatusBadRequest)
-//		return
-//	}
-//
-//	_, err = file.Seek(start, io.SeekStart)
-//	if err != nil {
-//		fmt.Println(err.Error())
-//		ErrorHandler(w, fmt.Sprintf("Could not get resource %v", fileIdString), http.StatusInternalServerError)
-//		return
-//	}
-//
-//	addPartialContentHeader(w, start, end, fileSize)
-//	w.Header().Add("Content-Disposition", fmt.Sprintf("attachment; filename=\"%v\"", continuousFileInTree.Name))
-//
-//	mimeType := utilities.GetContentTypeHeaderMimeType(continuousFileInTree)
-//	w.Header().Add("Content-Type", mimeType)
-//	w.WriteHeader(http.StatusPartialContent)
-//	io.CopyN(w, file, end-start)
-//}
-
 func getFileSize(file *os.File) (int64, error) {
 	fileInfo, err := file.Stat()
 	if err != nil {
