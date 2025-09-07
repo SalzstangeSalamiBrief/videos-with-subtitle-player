dbe19afc73e940803261a757053588842cd839a6
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2025-09-07 09:19:50 +0200

    Add mutex to prevent parallel execution of webp conversion

--- backend/cmd/webpTransformerJob/main.go
+++ backend/cmd/webpTransformerJob/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 )
@@ -20,9 +21,16 @@ func main() {
 	ticker := time.NewTicker(time.Duration(initializedConfiguration.GetExecutionIntervalInMinutes()) * time.Minute)
 	quitChannel := make(chan struct{})
 
+	var conversionMutex sync.Mutex
+
 	for {
 		select {
 		case <-ticker.C:
+			if !conversionMutex.TryLock() {
+				log.Printf("Conversion is already running. Skip duplicate execution.")
+				continue
+			}
+
 			log.Printf("Start webp transformer at '%v'\n", time.Now())
 			err := webp.ExecuteWebpConversion(webp.ExecuteWebpConversionConfiguration{RootPath: initializedConfiguration.GetRootPath(), ShouldDeleteNonWebpImages: initializedConfiguration.GetShouldDeleteNonWebpImages()})
 			if err != nil {
@@ -31,8 +39,12 @@ func main() {
 			}
 
 			log.Printf("Finish webp transformer at '%v'\n", time.Now())
+			conversionMutex.Unlock()
+
 		case <-osExitChannel:
 		case <-quitChannel:
+			close(quitChannel)
+			close(osExitChannel)
 			ticker.Stop()
 			os.Exit(0)
 		}
