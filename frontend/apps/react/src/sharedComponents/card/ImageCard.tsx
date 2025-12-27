e317a54cd5237d88377833f9db6d408ed3dc5258
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2025-12-27 08:43:44 +0100

    Remove unused code from ImageCard

--- frontend/apps/react/src/sharedComponents/card/ImageCard.tsx
+++ frontend/apps/react/src/sharedComponents/card/ImageCard.tsx
@@ -18,7 +18,6 @@ interface ICardProps {
 }
 
 export function ImageCard({ title, imageUrls, linkOptions }: ICardProps) {
-  // TODO DOES NOT USE THE WHOLE HEIGHT
   return (
     <>
       <article className={`card image-full ${styles.card}`}>
@@ -46,47 +45,7 @@ export function ImageCard({ title, imageUrls, linkOptions }: ICardProps) {
             </h1>
           </header>
         </div>
-        {/* <div className="card-body">
-          <h2 className="card-title">{title}</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div> */}
       </article>
-      {/* <article
-        className={`${styles.card} ${imageUrls ? '' : 'bg-fuchsia-800'}`}
-      >
-        {imageUrls && (
-          <ProgressiveImage
-            {...imageUrls}
-            alt={`Cover image of the item ${title}`}
-          />
-        )}
-        <div role="presentation">
-          <div
-            role="presentation"
-            className="flex h-full items-end bg-linear-to-t from-slate-800 from-[5ch] to-[20ch]"
-          >
-            <header className="h-fit overflow-hidden rounded-b-md p-4">
-              <h1
-                className="max-w-text line-clamp-3 overflow-hidden font-bold text-ellipsis whitespace-normal"
-                title={title}
-              >
-                <TanStackRouterLink
-                  {...linkOptions}
-                  className={`${baseLinkStyles} block h-full w-full`}
-                >
-                  {title}
-                </TanStackRouterLink>
-              </h1>
-            </header>
-          </div>
-        </div>
-      </article> */}
     </>
   );
 }
